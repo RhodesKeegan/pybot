a629d8cf7c3d3470cdbe013232cfba84666adbe2
RhodesKeegan <rhodesk@msoe.edu>
2021-12-26 23:08:27 -0600

    Added functionality for gamestop website, the scrape will run and will refresh the page every second. If the scrape finds that there is a series x in stock, it will halt the execution of the scrape method for the next 30 minutes.

--- bot.py
+++ bot.py
@@ -1,32 +1,30 @@
 import discord as ds
+import asyncio
+from datetime import datetime
 import os
+from discord.ext import tasks, commands
 from scraper import Scraper
 from dotenv import load_dotenv
 
 load_dotenv()
-
 client = ds.Client()
-
 scraper = Scraper()
 
 @client.event
 async def on_ready():
     print(f'{client.user} is now online!')
+    scrape.start()
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    content = message.content.lower()
+@tasks.loop(seconds=1.0)
+async def scrape():
+    in_stock = scraper.check_availability()
+    if 'add to cart' in in_stock.lower():
+        channel = client.get_channel(921496506077438025)
+        await channel.send(scraper.url)
+        await asyncio.sleep(1800)
 
-    if f'$search' in content:
-        key_words, search_words = scraper.keywords_search_words(content)
-        result_links = scraper.search(key_words)
-        links = scraper.send_link(result_links, search_words)
-        if len(links) > 0:
-            for link in links:
-                await message.channel.send(link)
-        else:
-            await message.channel.send('No results found :(')
+@scrape.before_loop
+async def before_scrape():
+    await client.wait_until_ready()
 
 client.run(os.getenv('TOKEN'))
--- scraper.py
+++ scraper.py
@@ -4,24 +4,10 @@ import requests
 class Scraper:
     def __init__(self):
         self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36'}
-        self.url = 'https://runpee.com/?s='
+        self.url = 'https://www.gamestop.com/products/microsoft-xbox-series-x/224744.html'
 
-    def keywords_search_words(self, message):
-        words = message.split()[1:]
-        keywords = '+'.join(words)
-        search_words = ' '.join(words)
-        return keywords, search_words
-
-    def search(self, keywords):
-        response = requests.get(self.url+keywords, headers=self.headers)
+    def check_availability(self):
+        response = requests.get(self.url, headers=self.headers)
         soup = BeautifulSoup(response.content, 'html.parser')
-        result_links = soup.findAll('a')
-        return result_links
-
-    def send_link(self, result_links, search_words):
-        send_link = set()
-        for link in result_links:
-            text = link.text.lower()
-            if search_words in text:
-                send_link.add(link.get('href'))
-        return send_link
+        button = soup.find(id='add-to-cart-buttons')
+        return button.get_text()
